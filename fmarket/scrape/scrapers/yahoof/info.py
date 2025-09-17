a6bd702a8200db961f964f5232832b246c796943
faalbers <chalbers@gmail.com>
2025-09-17 10:02:42 -0700

    implemented yahoof scrapers

--- fmarket/scrape/scrapers/yahoof/info.py
+++ fmarket/scrape/scrapers/yahoof/info.py
@@ -1,16 +1,103 @@
 from .yahoof import YahooF
 from ....database import Database
-import logging
+import logging, time
+from pprint import pp
+from datetime import datetime
+import pandas as pd
+from ....utils import FTime
 
 class YahooF_Info(YahooF):
     db_name = 'yahoof_info'
 
     def __init__(self):
+        super().__init__()
         self.db = Database(self.db_name)
 
     def scrape_data(self, key_values=[]):
-        logger = logging.getLogger('YahooF_Info'.ljust(20, ' '))
-        logger.info(self.db_name)
+        # check status
+        symbols, info = self.scrape_status(key_values=key_values)
+        if len(symbols) == 0: return
+
+        self.logger = logging.getLogger('YahooF_Info'.ljust(20, ' '))
+
+        self.logger.info('start update')
+        
+        # backup first
+        self.logger.info(self.db.backup())
+
+        procs = {
+            'info': self.proc_info,
+        }
+        self.multi_exec(procs, symbols)
+
+        self.logger.info('update done')
+    
+    def proc_info(self,ticker):
+        data = None
+        while True:
+            try:
+                info = ticker.info
+                if not isinstance(info, type(None)) and 'quoteType' in info:
+                    data = info
+                # else:
+                #     data = 'info is None'
+            except Exception as e:
+                if str(e) == 'Too Many Requests. Rate limited. Try after a while.':
+                    self.logger.info('Rate Limeit: wait 60 seconds')
+                    time.sleep(60)
+                    continue
+                else:
+                    pass
+                    # data[2]['info'] = str(e)
+            break
+        return data
+
+    def push_api_data(self, symbol, response_data):
+        ftime = FTime()
+        result_data = response_data['info']
+
+        status = pd.DataFrame({'info': 0}, index=[symbol])
+        status.index.name = 'symbol'
+
+        valid = False
+        if not isinstance(result_data, type(None)):
+            # clean up some stuff
+            if 'companyOfficers' in result_data: result_data.pop('companyOfficers')
+            if 'executiveTeam' in result_data: result_data.pop('executiveTeam')
+            if 'symbol' in result_data: result_data.pop('symbol')
+            result = pd.DataFrame([result_data], index=[symbol])
+            result.index.name = 'symbol'
+            self.db.table_write('info', result)
+            status.loc[symbol, 'info'] = int(ftime.now_local.timestamp())
+            valid = True
+
+        # update status
+        self.db.table_write('status_db', status)
+
+        return valid
+
+    def scrape_status(self, key_values=[], tabs=0):
+        # timestamps
+        ftime = FTime()
+        five_days_ts = ftime.get_offset(ftime.now_local, days=-5).timestamp()
+
+        status_db = self.db.table_read('status_db')
+        tabs_string = '  '*tabs
+        info = '%sdatabase: %s\n' % (tabs_string, self.db_name)
+        status = []
+        if status_db.shape[0] > 0:
+            symbols_skip = status_db['info'] == 0 # skip symbols that did not work last time
+            symbols_skip |= status_db['info'] >= five_days_ts # skip symbols that were done within the last 5 days
+            status = sorted(set(key_values).difference(status_db[symbols_skip].index))
+            info += '%s  table: info:\n' % (tabs_string)
+            info += '%s    update     : %s symbols\n' % (tabs_string, len(status))
+        else:
+            # we add all key_values to status
+            status = key_values
+            info += '%s  table: info:\n' % (tabs_string)
+            info += '%s    update     : %s symbols\n' % (tabs_string, len(status))
+            info += '%s    update     : Not scraped before\n' % (tabs_string)
+        return status, info
 
     def get_vault_data(self, data_name, columns, key_values):
         if data_name == 'info':
--- fmarket/scrape/scrapers/yahoof/yahoof.py
+++ fmarket/scrape/scrapers/yahoof/yahoof.py
@@ -1,4 +1,67 @@
+from ratelimit import limits, sleep_and_retry
+from ....utils import Stop
+import yfinance as yf
+import logging
+
 class YahooF():
+    @sleep_and_retry
+    @limits(calls=100, period=60) # 6000/hour
+    def exec_proc(self, proc, arguments):
+        return proc(**arguments)
+
+    def yfinancetest(self):
+        ticker = yf.Ticker('AAPL')
+        try:
+            quoteType = ticker.fast_info['quoteType']
+            if len(quoteType) == 0: return False
+        except Exception as e:
+            if str(e) == 'Too Many Requests. Rate limited. Try after a while.':
+                print('Too Many Requests. Rate limited. Change VPN location')
+                return False
+        return True
+    
     def __init__(self):
-        pass
+        # make yfinance non verbose
+        yflogger = logging.getLogger('yfinance')
+        yflogger.disabled = True
+        yflogger.propagate = False
+
+    def multi_exec(self, procs, symbols):
+        stop = Stop()
+        count_done = 0
+        failed = 0
+        failed_total = 0
+        for symbol in symbols:
+            if (count_done % 100) == 0:
+                self.logger.info('to do: %s , failed: %s' % (len(symbols)-count_done, failed))
+                self.db.commit()
+                failed = 0
+            
+            ticker = yf.Ticker(symbol)
+            data = {}
+            for data_name, proc in procs.items():
+                arguments = {'ticker': ticker}
+                data[data_name] = self.exec_proc(proc, arguments)
+
+            if not self.push_api_data(symbol, data):
+                failed += 1
+                failed_total += 1
+            
+            count_done += 1
+            
+            # manually stop if needed
+            if stop.is_set:
+                self.logger.info('manually stopped multi_exec')
+                self.db.commit()
+                break
+            
+            # run a yfinance test every 100 entriesexec entries
+            if (count_done % 100) == 0:
+                if not self.exec_proc(self.yfinancetest, {}):
+                    self.logger.info('yfinance not ok ...')
+                    break
+                else:
+                    self.logger.info('yfinance still ok ...')
+        
+        self.logger.info('done: %s , failed: %s' % (count_done, failed_total))
     
