d9d551e2b8b5dcd11c5ce84195da4a59cd35a658
faalbers <chalbers@gmail.com>
2025-09-17 10:03:44 -0700

    implemented fmp scrapers

--- fmarket/scrape/scrapers/fmp/fmp.py
+++ fmarket/scrape/scrapers/fmp/fmp.py
@@ -1,4 +1,28 @@
+import requests
+from data.keys import KEYS
+from ratelimit import limits, sleep_and_retry
+
 class FMP():
     def __init__(self):
-        pass
+        self.init_session()
+
+    def init_session(self):
+        # create session
+        params = {'apikey': KEYS['FMP']['KEY']}
+        self.session = requests.Session()
+        self.session.params.update(params)
+
+    @sleep_and_retry
+    @limits(calls=250, period=86400)
+    def session_get(self, request_arguments):
+        return self.session.get(**request_arguments)
     
+    def request(self, request_arguments):
+        response = self.session_get(request_arguments)
+        if response.headers.get('content-type').startswith('application/json'):
+            response_data = response.json()
+            self.push_api_data(response_data)
+        else:
+            self.logger.info('unknown request response')
+            self.logger.info('status code: %s' % (response.status_code))
+            self.logger.info('content type: %s' % (response.status_code))
--- fmarket/scrape/scrapers/fmp/stocklist.py
+++ fmarket/scrape/scrapers/fmp/stocklist.py
@@ -1,33 +1,77 @@
 from .fmp import FMP
 from ....database import Database
+from ....utils import FTime
 from datetime import datetime
 import logging
+import pandas as pd
 
 class FMP_Stocklist(FMP):
     db_name = 'fmp_stocklist'
 
     def __init__(self):
+        super().__init__()
         self.db = Database(self.db_name)
 
     def scrape_data(self, key_values=[]):
+        # check status
+        status, info = self.scrape_status(key_values=key_values)
+        if not status: return
+
         # create logger with at least 20 character logger name
-        logger = logging.getLogger('FMP_Stocklist'.ljust(20, ' '))
-        logger.info(self.db_name)
+        self.logger = logging.getLogger('FMP_Stocklist'.ljust(20, ' '))
+
+        self.logger.info('start update')
+        
+        # backup first
+        self.logger.info(self.db.backup())
+
+        request_arguments = {
+            'url': 'https://financialmodelingprep.com/api/v3/stock/list',
+            # 'params': {},
+            'timeout': 30,
+        }
+        self.request(request_arguments)
+        
+        # update status
+        ftime = FTime()
+        status_db = pd.DataFrame([{'timestamp': int(ftime.now_local.timestamp())}], index=['stocklist'])
+        status_db.index.name = 'table_name'
+        self.db.table_write('status_db', status_db)
+
+        self.logger.info('update done')
+
+    def push_api_data(self, response_data):
+        write_data =  []
+        write_symbols = []
+        exchange_us = ['NYSE', 'NASDAQ', 'PNK', 'OTC', 'AMEX', 'CBOE']
+        for entry in response_data:
+            if not entry['exchangeShortName'] in exchange_us: continue
+            symbol = entry.pop('symbol').upper()
+            write_data.append(entry)
+            write_symbols.append(symbol)
+        
+        write_data = pd.DataFrame(write_data, index=write_symbols)
+        write_data.index.name = 'symbol'
+        self.db.table_write('stocklist', write_data, replace=True)
 
     def scrape_status(self, key_values=[], tabs=0):
+        # get timestamps
+        ftime = FTime()
+        now = ftime.now_local
+
         status_db = self.db.table_read('status_db')
         tabs_string = '  '*tabs
         info = '%sdatabase: %s\n' % (tabs_string, self.db_name)
         status = None
         if status_db.shape[0] > 0:
-            timestamp_pdt = int(datetime.now().timestamp())
-            timestamp_last = status_db.loc['stocklist', 'timestamp']
-            timestamp_next = timestamp_last - (3600 * 24 * 182)
-            status = timestamp_next > timestamp_last
+            last_ts = status_db.loc['stocklist', 'timestamp']
+            last_time = ftime.get_from_ts_local(last_ts)
+            next_time = ftime.get_offset(last_time, months=6)
+            status = now >= next_time
             info += '%s  table: stocklist\n' % (tabs_string)
             info += '%s    update     : %s\n' % (tabs_string, status)
-            info += '%s    last update: %s\n' % (tabs_string, datetime.fromtimestamp(timestamp_last))
-            info += '%s    next update: %s\n' % (tabs_string, datetime.fromtimestamp(timestamp_next))
+            info += '%s    last update: %s\n' % (tabs_string, last_time)
+            info += '%s    next update: %s\n' % (tabs_string, next_time)
         else:
             status = True
             info += '%s  table: stocklist:\n' % (tabs_string)
