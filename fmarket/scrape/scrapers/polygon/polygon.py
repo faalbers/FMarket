fed04ce0241a808832df503b61f696cfbacc258f
faalbers <chalbers@gmail.com>
2025-09-17 10:03:12 -0700

    implemented polygon scrapers

--- fmarket/scrape/scrapers/polygon/polygon.py
+++ fmarket/scrape/scrapers/polygon/polygon.py
@@ -1,4 +1,52 @@
+import requests
+from data.keys import KEYS
+from ratelimit import limits, sleep_and_retry
+from pprint import pp
+from ....utils import Stop
+
 class Polygon():
     def __init__(self):
-        pass
+        self.init_session()
+
+    def init_session(self):
+        # create session
+        params = {'apikey': KEYS['POLYGON']['KEY']}
+        self.session = requests.Session()
+        self.session.params.update(params)
+
+    @sleep_and_retry
+    @limits(calls=5, period=70)
+    def session_get(self, request_arguments):
+        return self.session.get(**request_arguments)
+    
+    # handle requests till they are exhausted
+    def request(self, request_arguments, push_proc):
+        stop = Stop()
+        next_request_arguments = request_arguments
+        entries = 0
+        # runs = 10
+        while next_request_arguments:
+            response = self.session_get(next_request_arguments)
+            if response.headers.get('content-type').startswith('application/json'):
+                write_data = {}
+                response_data = response.json()
+                if 'results' in response_data:
+                    push_proc(response_data['results'])
+                else:
+                    self.logger.info('no result in response chunk, stopping requests')
+                    break
+                if 'count' in response_data:
+                    entries += response_data['count']
+                self.logger.info('entries found: %s' % entries)
+                next_request_arguments = None
+                if 'next_url' in response_data:
+                    next_request_arguments = {'url': response_data['next_url']}
+            else:
+                next_request_arguments = None
+            
+            if stop.is_set:
+                self.logger.info('manually stopped request')
+                self.logger.info('entries found: %s' % entries)
+                self.db.commit()
+                next_request_arguments = None
     
--- fmarket/scrape/scrapers/polygon/tickers.py
+++ fmarket/scrape/scrapers/polygon/tickers.py
@@ -1,32 +1,127 @@
 from .polygon import Polygon
 from ....database import Database
+from ....utils import FTime
 from datetime import datetime
 import logging
+import pandas as pd
 
 class Polygon_Tickers(Polygon):
     db_name = 'polygon_tickers'
 
     def __init__(self):
+        super().__init__()
         self.db = Database(self.db_name)
 
     def scrape_data(self, key_values=[]):
-        logger = logging.getLogger('Polygon_Tickers'.ljust(20, ' '))
-        logger.info(self.db_name)
-    
+        # check status
+        status, info = self.scrape_status(key_values=key_values)
+        if not status: return
+
+        self.logger = logging.getLogger('Polygon_Tickers'.ljust(20, ' '))
+
+        self.logger.info('start update')
+        
+        # backup first
+        self.logger.info(self.db.backup())
+
+        # get tickers
+        request_arguments = {
+            'url': 'https://api.polygon.io/v3/reference/tickers',
+            'params': {
+                'limit': 1000,
+            },
+        }
+
+        self.request(request_arguments, self.push_tickers_data)
+
+        # get types
+        request_arguments = {
+                'url': 'https://api.polygon.io/v3/reference/tickers/types',
+        }
+
+        self.request(request_arguments, self.push_types_data)
+
+        # update status
+        ftime = FTime()
+        status_db = pd.DataFrame([{'timestamp': int(ftime.now_local.timestamp())}], index=['tickers'])
+        status_db.index.name = 'table_name'
+        self.db.table_write('status_db', status_db)
+
+        self.logger.info('update done')
+
+    def push_tickers_data(self, response_data):
+        write_data =  []
+        write_symbols = []
+        for entry in response_data:
+            # do not include these markets
+            if entry['market'] in ['fx', 'crypto']: continue
+            symbol = entry.pop('ticker').upper()
+
+            # create correct ticker symbol
+            if entry['market'] == 'stocks' and not symbol.isalpha() and 'type' in entry:
+                if 'type' in entry and entry['type'] == 'UNIT':
+                    # handle unit
+                    symbol = symbol.replace('.U', '-UN')
+                elif 'type' in entry and entry['type'] == 'WARRANT':
+                    # handle warrant
+                    symbol = symbol.replace('.WS.A', '-WT')
+                    symbol = symbol.replace('.WS', '-WT')
+            if entry['market'] == 'indices':
+                # handle indices
+                symbol = symbol.replace('I:', '^')
+                entry['type'] = 'IX'
+            if len(symbol) > 2 and symbol[-2] == '.':
+                if symbol[-1] <= 'C':
+                    # handle class ticker
+                    symbol = symbol.replace('.', '-')
+                else:
+                    # handle other
+                    symbol = symbol.replace('.', '')
+
+            write_data.append(entry)
+            write_symbols.append(symbol)
+        
+        write_data = pd.DataFrame(write_data, index=write_symbols)
+        write_data = write_data.reset_index().drop_duplicates(subset='index', keep='last').set_index('index')
+        write_data.index.name = 'symbol'
+        self.db.table_write('tickers', write_data)
+
+        # update on every page to not loose data
+        self.db.commit()
+
+    def push_types_data(self, response_data):
+        write_data =  []
+        write_symbols = []
+        for entry in response_data:
+            symbol = entry.pop('code')
+            write_data.append(entry)
+            write_symbols.append(symbol)
+        
+        write_data = pd.DataFrame(write_data, index=write_symbols)
+        write_data.index.name = 'code'
+        self.db.table_write('types', write_data)
+
+        # update on every page to not loose data
+        self.db.commit()        
+
     def scrape_status(self, key_values=[], tabs=0):
+        # get timestamps
+        ftime = FTime()
+        now = ftime.now_local
+
         status_db = self.db.table_read('status_db')
         tabs_string = '  '*tabs
         info = '%sdatabase: %s\n' % (tabs_string, self.db_name)
         status = None
         if status_db.shape[0] > 0:
-            timestamp_pdt = int(datetime.now().timestamp())
-            timestamp_last = status_db.loc['tickers', 'timestamp']
-            timestamp_next = timestamp_last - (3600 * 24 * 182)
-            status = timestamp_next > timestamp_last
+            last_ts = status_db.loc['tickers', 'timestamp']
+            last_time = ftime.get_from_ts_local(last_ts)
+            next_time = ftime.get_offset(last_time, months=6)
+            status = now >= next_time
             info += '%s  table: tickers\n' % (tabs_string)
             info += '%s    update     : %s\n' % (tabs_string, status)
-            info += '%s    last update: %s\n' % (tabs_string, datetime.fromtimestamp(timestamp_last))
-            info += '%s    next update: %s\n' % (tabs_string, datetime.fromtimestamp(timestamp_next))
+            info += '%s    last update: %s\n' % (tabs_string, last_time)
+            info += '%s    next update: %s\n' % (tabs_string, next_time)
         else:
             status = True
             info += '%s  table: tickers:\n' % (tabs_string)
