d08cf8447d309c25eb31d17775bf6c50d525ac90
faalbers <chalbers@gmail.com>
2025-11-02 13:36:03 -0800

    fixed some params info

--- fmarket/analysis/analysis_params.py
+++ fmarket/analysis/analysis_params.py
@@ -146,45 +146,53 @@ its short-term debt obligations using only its most liquid assets: cash and cash
 This metric assesses if a company can meet its immediate debts using only cash and near-cash assets. 
 If it's too high it does not take up enough liabilities for growth and is too conservative""",
             'unit': '%',
-            'guidance': 'Should be between 50 and 100',
+            'guidance': 'Should be between 50 and 100. Lower cant cover short term debt and higher is too conservative',
         },
         'gross_profit_margin': {
             'info':
-"""Gross Profit Margin is a fundamental financial metric that gauges a company's profitability and operational efficiency.
+"""Gross Profit = Revenue - Cost of Goods Sold
+Gross Profit Margin is a fundamental financial metric that gauges a company's profitability and operational efficiency.
 It is the percentage of revenue that a company retains after subtracting the direct costs associated with producing or
 delivering its goods or services, known as the Cost of Goods Sold (COGS).""",
+            'formula': '(gross_profit/total_revenue)',
             'unit': '%',
             'guidance': 'Should be 25 or up',
         },
-        'net_profit_margin': {
-            'info':
-"""Net Profit Margin is a crucial financial ratio that indicates how much profit a company retains for each dollar of
-revenue generated after accounting for all expenses, including operating costs, interest, and taxes. It's a key
-indicator of a company's overall financial health and operational efficiency. A higher net profit margin generally
-suggests that a company is managing its costs effectively and generating substantial profits from its operations.
-Conversely, a lower margin may indicate financial struggles, weak pricing strategies, or inefficiencies.""",
-            'unit': '%',
-            'guidance': 'Should be 15 or up',
-        },
         'operating_profit_margin': {
             'info':
-"""Operating Profit Margin, also called operating profit margin or return on sales (ROS), is a financial ratio that measures
+"""Operating Profit = Gross Profit - Operating Expenses
+Operating Profit Margin, also called operating profit margin or return on sales (ROS), is a financial ratio that measures
 a company's profitability and operational efficiency. It indicates how much profit a company makes from its
 core business operations after deducting the direct and indirect costs associated with those operations,
 relative to its total revenue.""",
+            'formula': '(operating_profit/total_revenue)',
             'unit': '%',
             'guidance': 'Should be 25 or up',
         },
         'profit_margin': {
             'info':
-"""Profit Margin, also called profit margin or return on earnings (ROE), is a crucial financial ratio that indicates how much
+"""Profit = Operating Profit - Interest Expenses
+Profit Margin, also called profit margin or return on earnings (ROE), is a crucial financial ratio that indicates how much
 profit a company retains for each dollar of revenue generated after accounting for all expenses, including operating costs,
 interest, taxes are NOT included. It's a key indicator of a company's overall financial health and operational efficiency.
 A higher profit margin generally suggests that a company is managing its costs effectively and generating substantial profits
 from its operations.
 Conversely, a lower margin may indicate financial struggles, weak pricing strategies, or inefficiencies.""",
+            'formula': '(profit/total_revenue)',
             'unit': '%',
             'guidance': 'Should be 20 or up',
+        },
+        'net_profit_margin': {
+            'info':
+"""Net Profit = Profit - Taxes
+Net Profit Margin is a crucial financial ratio that indicates how much profit a company retains for each dollar of
+revenue generated after accounting for all expenses, including operating costs, interest, and taxes. It's a key
+indicator of a company's overall financial health and operational efficiency. A higher net profit margin generally
+suggests that a company is managing its costs effectively and generating substantial profits from its operations.
+Conversely, a lower margin may indicate financial struggles, weak pricing strategies, or inefficiencies.""",
+            'formula': '(net_profit/total_revenue)',
+            'unit': '%',
+            'guidance': 'Should be 15 or up',
         },
         'dividends': {
             'info':
