b3155ab74c977f43502c77ef5623c2f0b1c8b1de
faalbers <chalbers@gmail.com>
2026-05-09 10:35:55 -0700

    added data corrution check and some cleanup

--- fmarket/portfolio/account.py
+++ fmarket/portfolio/account.py
@@ -7,22 +7,23 @@ class Account:
     def __init__(self, id, description=None, data=None):
         self.id = id
         self.description = description
-        self.positions = pd.DataFrame()
-        self.transactions = pd.DataFrame()
+        self.__positions = pd.DataFrame()
+        self.__transactions = pd.DataFrame()
         if isinstance(data, dict):
             self.__update_account(data)
         else:
             db = Database('portfolio')
-            self.positions = db.table_read('positions_%s' % self.id)
-            if 'date' in self.positions.columns:
-                self.positions['date'] = pd.to_datetime(self.positions['date'], unit='s')
-            self.transactions = db.table_read('transactions_%s' % self.id)
-            self.transactions['date'] = pd.to_datetime(self.transactions['date'], unit='s')
+            self.__positions = db.table_read('positions_%s' % self.id)
+            if 'date' in self.__positions.columns:
+                self.__positions['date'] = pd.to_datetime(self.__positions['date'], unit='s')
+            self.__transactions = db.table_read('transactions_%s' % self.id)
+            self.__transactions['date'] = pd.to_datetime(self.__transactions['date'], unit='s')
+            self.__check_data_corruption()
             self.__fix_quantity_out()
             del(db)
 
     def get_symbols(self):
-       return sorted(self.positions.index)
+       return sorted(self.__positions.index)
     
     def get_positions(self):
         positions = {
@@ -32,13 +33,14 @@ class Account:
 
         return positions
 
+    def get_transactions(self):
+        return self.__transactions.copy()
+
     def __get_history(self):
         history_symbols = {}
-        symbols = self.transactions['security_symbol'].unique()
+        symbols = self.__transactions['security_symbol'].unique()
         for symbol in symbols:
-            transactions = self.transactions[self.transactions['security_symbol'] == symbol].copy()
-            # if self.id == '151827600' and symbol == 'CWEN':
-            #     print(transactions)
+            transactions = self.__transactions[self.__transactions['security_symbol'] == symbol].copy()
 
             has_no_quantity = transactions['quantity'].isna() | (transactions['quantity'] == 0)
             
@@ -76,35 +78,9 @@ class Account:
 
         return history_symbols
         
-    def __get_history_old(self):
-        history_symbols = {}
-        symbols = self.transactions['security_symbol'].unique()
-        for symbol in symbols:
-            transactions = self.transactions[self.transactions['security_symbol'] == symbol]
-            if self.id == '151827600' and symbol == 'MVRXX':
-                print(transactions)
-
-            history = transactions[['date', 'amount', 'quantity']].copy()
-            is_dividend = transactions['action'].isin(['dividend', 'dividend qualified'])
-            history.loc[is_dividend, 'dividend'] = history.loc[is_dividend, 'amount']
-            is_cap_gain = transactions['action'].str.startswith('cap gain')
-            history.loc[is_cap_gain, 'cap_gain'] = history.loc[is_cap_gain, 'amount']
-            is_reinvest = transactions['action'] == 'reinvest'
-            history.loc[is_reinvest, 'reinvest'] = history.loc[is_reinvest, 'amount']
-            history['distribution'] = history['dividend'].replace(np.nan,0) + history['cap_gain'].replace(np.nan,0)
-            history['distribution'] = history['distribution'].replace(0, np.nan)
-            history.set_index('date', inplace=True)
-            history.index = history.index.date
-            history.dropna(axis=0, how='all', inplace=True)
-            history = history.groupby(history.index).sum()
-
-            history_symbols[symbol] = history
-
-        return history_symbols
-
     def __get_positions(self):
         now = FTime().now_naive
-        positions = self.positions.sort_values('cost', ascending=False).copy()
+        positions = self.__positions.sort_values('cost', ascending=False).copy()
         positions['alloc_%'] = ((positions['cost'] / positions['cost'].sum()) * 100).round(2)
         if 'date' in positions.columns:
             positions['years'] = ((now - positions['date']).dt.days / 365.0).round(2)
@@ -128,9 +104,18 @@ class Account:
 
     def __fix_quantity_out(self):
         # fix quantity out bug
-        for description in self.transactions[self.transactions['action'] == 'quantity out']['description'].unique():
-            quantity_in = self.transactions[(self.transactions['action'] == 'quantity in') & (self.transactions['description'] == description)]
+        for description in self.__transactions[self.__transactions['action'] == 'quantity out']['description'].unique():
+            quantity_in = self.__transactions[(self.__transactions['action'] == 'quantity in') & (self.__transactions['description'] == description)]
             symbol = quantity_in['security_symbol'].unique()[0]
-            quantity_out = self.transactions[(self.transactions['action'] == 'quantity out') & (self.transactions['description'] == description)]
+            quantity_out = self.__transactions[(self.__transactions['action'] == 'quantity out') & (self.__transactions['description'] == description)]
             if quantity_out.index.shape[0] > 0:
-                self.transactions.loc[quantity_out.index, 'security_symbol'] = symbol
+                self.__transactions.loc[quantity_out.index, 'security_symbol'] = symbol
+
+    def __check_data_corruption(self):
+        if self.id != '151827600': return
+        def check_symbol_transactions(group):
+            if group.shape[0] > 1:
+                print('Corrupted transactions in account %s' % self.id)
+                print(self.__transactions.loc[group.index])
+                raise ValueError('Corrupted transactions')
+        self.__transactions.groupby(['security_symbol', 'description', 'amount']).apply(check_symbol_transactions, include_groups=False)
